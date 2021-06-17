01e2f3405ef03ed551e8e716d58011f0f56cf485
alexander eades <alexander.eades@student.centralia.edu>
2021-06-16 21:41:33 -0700

    Add project files.

---
+++ Eades_CashFlowManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eades_CashFlowManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Weekly Cash Flow Analysis is as follows:");
+        }
+    }
+}
---
+++ Eades_CashFlowManager/iPayable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eades_CashFlowManager
+{
+    interface iPayable
+    {
+        public decimal GetPayableAmmount();
+    }
+}
